eae33084f15a8c9374fb45a9331c5735b3df74bd
Shane Conn <shaneconn23@gmail.com>
2022-05-07 09:40:37 -0400

    let and const

---
+++ app.js
@@ -0,0 +1,25 @@
+// ES5 Global Constants
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+
+// ES2015 Global Constants
+/* Write an ES2015 Version */
+const PI = 3.14;
+PI = 42; //Error
+
+
+
+// Quiz
+// What is the difference between var and let?
+// var is function scope and let is block scope.
+
+// What is the difference between var and const?
+// var uses function scope and const uses block scope.
+// var can be reassigned and redeclared and const cant. 
+
+// What is the difference between let and const?
+// let can be reassigned and const cant.
+
+// What is hoisting?
+// In JavaScript, Hoisting is the default behavior of moving all the declarations at the top of the scope before code execution
